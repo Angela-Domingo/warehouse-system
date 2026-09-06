c76a8f33fb4886d8000acac00453ae62a696bd84
Angela Domingo <angeladomingo2006@gmail.com>
2026-09-06 22:25:54 +0530

    Add authentication API routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { register, login, getMe } = require('../controllers/authController');
+const { registerValidator, loginValidator } = require('../utils/validators/authValidators');
+const validate = require('../middleware/validate');
+const { protect } = require('../middleware/auth');
+
+router.post('/register', registerValidator, validate, register);
+router.post('/login', loginValidator, validate, login);
+router.get('/me', protect, getMe);
+
+module.exports = router;
